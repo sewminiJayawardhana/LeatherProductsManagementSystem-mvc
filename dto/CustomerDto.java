cf3a856cb09f4e465e1f21fec7744fa8a2839ee9
sewminiJyawardhana <139870212+sewminiJayawardhana@users.noreply.github.com>
2023-12-16 21:48:04 +0530

    Add files via upload

---
+++ dto/CustomerDto.java
@@ -0,0 +1,13 @@
+package lk.ijse.Shop.dto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerDto {
+    private String id;
+    private String name;
+    private String address;
+    private String email;
+
+}
